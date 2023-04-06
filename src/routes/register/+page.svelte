f450245300c30299c18010607d37a9cb42661e13
Flapperoo <gianandreb@gmail.com>
2023-04-06 17:28:54 +0800

    Adjusted white to ykwhite

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -4,34 +4,34 @@
 <div class="flex flex-row h-screen items-center justify-center subpixel-antialiased">
     <div class="bg-ykpurple flex flex-row rounded-xl">
         <div class="flex flex-col p-10 mx-4">
-            <span class="font-bold mb-5 text-4xl text-white -ml-4">REGISTER</span>
+            <span class="font-bold mb-5 text-4xl text-ykwhite -ml-4">REGISTER</span>
             <form>
                 <div class="gap-3 grid grid-cols-2">
                     <div class="form-control">
                         <label class="label" for="firstname">
-                            <span class="label-text text-lg text-white">First Name</span>
+                            <span class="label-text text-lg text-ykwhite">First Name</span>
                         </label>
                         <input type="text" name="firstname" placeholder="First Name" class="input input-bordered" />
                     </div>
                     <div class="form-control">
                         <label class="label" for="lastname">
-                            <span class="label-text text-lg text-white">Last Name</span>
+                            <span class="label-text text-lg text-ykwhite">Last Name</span>
                         </label>
                         <input type="text" name="lastname" placeholder="Last Name" class="input input-bordered" />
                     </div>
                     <div class="form-control col-span-2">
                         <label class="label" for="email">
-                            <span class="label-text text-lg text-white">E-mail Address</span>
+                            <span class="label-text text-lg text-ykwhite">E-mail Address</span>
                         </label>
                         <input type="email" name="email" placeholder="E-mail Address" class="input input-bordered" />
                     </div>
                     <div class="form-control col-span-2">
                         <label class="label" for="password">
-                            <span class="label-text text-lg text-white">Password</span>
+                            <span class="label-text text-lg text-ykwhite">Password</span>
                         </label>
                         <input type="password" name="password" placeholder="Password" class="input input-bordered" />
                     </div>
-                    <button class="btn col-start-2 ml-auto mt-3 bg-yklightpurple text-white" type="submit">Submit</button>
+                    <button class="btn col-start-2 ml-auto mt-3 bg-yklightpurple text-ykwhite" type="submit">Submit</button>
                 </div>
             </form>
             <div class="divider"></div>

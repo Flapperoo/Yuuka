7e27748f1f33708cda7ee10e555cce886e5eb3b7
TilapiaMilkshake <gavino.paolo@gmail.com>
2023-04-06 14:11:29 +0800

    Initial Commit
    Added links to all of the UI pages based on figma.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
+<nav>
+    <a href="/register">Register</a> |
+    <a href="/login">Login</a> | 
+    <a href="/onboarding">Onboarding</a> |
+    <a href="/budgeting">Budgeting</a> | 
+    <a href="/fundingGoals">Funding Goals</a> | 
+    <a href="/transactions">Transactions</a> |
+    <a href="/billTracker">Bill Tracker</a> | 
+    <a href="/reminderSettings">Reminder Settings</a> |
+    <a href="/settings">Settings</a> | 
+    <a href="/categories">Categories</a> | 
+    <a href="/yuukaHelper">Yuuka Helper</a> | 
+</nav>
+<br/>
+<h1>UI Navigation</h1>
+<p>This is for the checking of individual UI pages without having to load one page to get to another.</p>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>

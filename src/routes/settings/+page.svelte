c997193bf2e71a6dad48cf63ff0b23955a4a752d
PapiRaffy129 <rodolf.alfonso@gmail.com>
2023-04-17 21:58:55 +0800

    YK-15 Settings - Initial Setup
    Initial Setup

---
+++ src/routes/settings/+page.svelte
@@ -0,0 +1,64 @@
+<script>
+</script>
+<div class="flex h-screen bg-ykdarkgray">
+<!-- SIDE NAV-BAR -->
+<div class="w-72 px-6 py-10 bg-ykpurple">
+    <img src="Yuuka_Logo.png" alt="yuuka logo" class="bg-cover bg-center justify-center drop-shadow-lg p-5 w-230 h-230 object-cover object-center"> <br>
+            <h1 class="font-bold text-center text-2xl">Welcome Back!</h1><br>
+            <h1 class="font-bold text-center text-xl">Sensei Master</h1><br>
+            <hr class="my-2">
+            <!--NAV LIST-->
+            <div class="p-1.5 mt-3 flex items-center rounded-md px-4 hover:bg-yklightpurple">
+                <ion-icon name="speedometer-sharp"></ion-icon>
+                <span class="text-[20px] ml-4 text-yklightblue "><a href="#">Dashboard</a></span>
+            </div>
+            <div class="p-1.5 mt-3 flex items-center rounded-md px-4 hover:bg-yklightpurple">
+                <ion-icon name="speedometer-sharp"></ion-icon>
+                <span class="text-[20px] ml-4 text-yklightblue"><a href="#">Yuuka Helper</a></span>
+            </div>
+            <div class="p-1.5 mt-3 flex items-center rounded-md px-4 hover:bg-yklightpurple">
+                <ion-icon name="speedometer-sharp"></ion-icon>
+                <span class="text-[20px] ml-4 text-yklightblue"><a href="#">Budgeting</a></span>
+            </div>
+            <div class="p-1.5 mt-3 flex items-center rounded-md px-4 hover:bg-yklightpurple">
+                <ion-icon name="speedometer-sharp"></ion-icon>
+                <span class="text-[20px] ml-4 text-yklightblue"><a href="#">Bill Tracker</a></span>
+            </div>
+            <div class="p-1.5 mt-3 flex items-center rounded-md px-4 hover:bg-yklightpurple">
+                <ion-icon name="speedometer-sharp"></ion-icon>
+                <span class="text-[20px] ml-4 text-yklightblue"><a href="#">Categories</a></span>
+            </div>
+            <div class="p-1.5 mt-3 flex items-center rounded-md px-4 hover:bg-yklightpurple">
+                <ion-icon name="speedometer-sharp"></ion-icon>
+                <span class="text-[20px] ml-4 text-yklightblue"><a href="#">Transactions</a></span>
+            </div>
+            <div class="p-1.5 mt-3 flex items-center rounded-md px-4 hover:bg-yklightpurple">
+                <ion-icon name="speedometer-sharp"></ion-icon>
+                <span class="text-[20px] ml-4 text-yklightblue"><a href="#">Settings</a></span>
+            </div>
+  </div>
+  <!--END OF SIDEBAR-->
+
+    <!-- MAIN CONTENT -->
+    <div class="flex-1 bg-gray-200">
+        <div class="p-8">
+            <!-- Title -->
+            <h1 class="text-3xl font-bold">Dashboard</h1>
+        </div>
+        <div class="flex">
+            <!-- Div Spanning Remaining Width -->
+            <div class="flex-1 p-5 bg-ykgray">
+                <p>this is Column</p>
+                    <div class="flex">
+                        <div class="flex-1 bg-ykpurple">
+                            <p>This is 1st column content</p>
+                        </div>
+                        <div class="flex-1 bg-ykpurple">
+                            <p>This is 2nd column content</p>
+                        </div>
+                    </div>
+            </div>
+        </div>
+    </div>
+    <!--END OF MAIN CONTENT-->
+</div>

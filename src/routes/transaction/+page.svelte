299e1c92996d02268131e86286900c45e189a397
PapiRaffy129 <rodolf.alfonso@gmail.com>
2023-04-22 14:38:19 +0800

    Update YK-11 Transactions
    fixed height issue

--- src/routes/transaction/+page.svelte
+++ src/routes/transaction/+page.svelte
@@ -45,129 +45,131 @@
         <div class="pl-6 py-6"> <!--WEBPAGE TITLE-->
             <h1 class="text-3xl font-bold">Transactions</h1>
         </div>
-        
-        <!--Transaction Content-->
-        <div class="flex flex-row gap-4 px-6">
-            <div class="w-4/5 h-full bg-ykgray overflow-y-auto rounded-xl pb-6">
-                <div class="pl-4 py-2 flex items-center justify-center"> <!--WEBPAGE TITLE-->
-                    <h1 class="text-3xl font-bold text-ykpurple">Your Transaction History</h1>
+        <div class="h-4/5">
+            <!--Transaction Content-->
+            
+            <div class="flex flex-row gap-4 px-6 h-full">
+                <div class="w-4/5 h-full bg-ykgray overflow-y-auto rounded-xl pb-6">
+                    <div class="pl-4 py-2 flex items-center justify-center"> <!--WEBPAGE TITLE-->
+                        <h1 class="text-3xl font-bold text-ykpurple">Your Transaction History</h1>
+                    </div>
+                    <!--Column 1-->
+                    <div class="px-5 overflow-y-auto">
+                        <div class="grid grid-cols-1 gap-3 grid-flow-row text-yklightpurple px-5 pt-2 w-auto overflow-auto">
+                            <!--Table 1-->
+                            <div class="py-2 flex"> <!--WEBPAGE TITLE-->
+                                <span class="text-xl font-bold text-ykpurple">April XX, XXXX</span>
+                            </div>
+                            <table class="table-auto border border-ykgray rounded-xl overflow-hidden">
+                                <tbody>
+                                    <tr class="bg-ykwhite text-ykpurple text-left">
+                                        <td class="w-1/7 px-4 py-3"><span class="bg-ykgreen text-ykwhite rounded-xl px-2 py-1">6</span></td>
+                                        <td class="w-1/7 px-4 py-3">Transaction Name</td>
+                                        <td class="w-1/7 px-4 py-3">Edit / Delete</td>
+                                        <td class="w-1/7 px-4 py-3">Transaction Category</td>
+                                        <td class="w-1/7 px-4 py-3">+Php15,000.00</td>
+                                        <td class="w-1/7 px-4 py-3">Up/Down</td>
+                                    </tr>
+                                    <tr class="bg-ykwhite text-ykpurple text-left">
+                                        <td class="w-1/7 px-4 py-3"><span class="bg-ykred text-ykwhite rounded-xl px-2 py-1">5</span></td>
+                                        <td class="w-1/7 px-4 py-3">Transaction Name</td>
+                                        <td class="w-1/7 px-4 py-3">Edit / Delete</td>
+                                        <td class="w-1/7 px-4 py-3">Transaction Category</td>
+                                        <td class="w-1/7 px-5 py-3">-Php1,000.00</td>
+                                        <td class="w-1/7 px-4 py-3">Up/Down</td>
+                                    </tr>
+                                    <tr class="bg-ykwhite text-ykpurple text-left">
+                                        <td class="w-1/7 px-4 py-3"><span class="bg-ykred text-ykwhite rounded-xl px-2 py-1">4</span></td>
+                                        <td class="w-1/7 px-4 py-3">Transaction Name</td>
+                                        <td class="w-1/7 px-4 py-3">Edit / Delete</td>
+                                        <td class="w-1/7 px-4 py-3">Transaction Category</td>
+                                        <td class="w-1/7 px-5 py-3">-Php1,500.00</td>
+                                        <td class="w-1/7 px-4 py-3">Up/Down</td>
+                                    </tr>
+                                </tbody>
+                                
+                                
+                            </table>
+                            <!--END OF TABLE 1-->
+                            <!--Table 1-->
+                            <div class="py-2 flex"> <!--WEBPAGE TITLE-->
+                                <span class="text-xl font-bold text-ykpurple">March XX, XXXX</span>
+                            </div>
+                            <table class="table-auto border border-ykgray rounded-xl overflow-hidden">
+                                <tbody>
+                                    <tr class="bg-ykwhite text-ykpurple text-left">
+                                        <td class="w-1/7 px-4 py-3"><span class="bg-ykgreen text-ykwhite rounded-xl px-2 py-1">3</span></td>
+                                        <td class="w-1/7 px-4 py-3">Transaction Name</td>
+                                        <td class="w-1/7 px-4 py-3">Edit / Delete</td>
+                                        <td class="w-1/7 px-4 py-3">Transaction Category</td>
+                                        <td class="w-1/7 px-4 py-3">+Php15,000.00</td>
+                                        <td class="w-1/7 px-4 py-3">Up/Down</td>
+                                    </tr>
+                                    <tr class="bg-ykwhite text-ykpurple text-left">
+                                        <td class="w-1/7 px-4 py-3"><span class="bg-ykred text-ykwhite rounded-xl px-2 py-1">2</span></td>
+                                        <td class="w-1/7 px-4 py-3">Transaction Name</td>
+                                        <td class="w-1/7 px-4 py-3">Edit / Delete</td>
+                                        <td class="w-1/7 px-4 py-3">Transaction Category</td>
+                                        <td class="w-1/7 px-5 py-3">-Php1,000.00</td>
+                                        <td class="w-1/7 px-4 py-3">Up/Down</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <!--END OF TABLE 1-->
+                            <!--Table 1-->
+                            <div class="py-2 flex"> <!--WEBPAGE TITLE-->
+                                <span class="text-xl font-bold text-ykpurple">February XX, XXXX</span>
+                            </div>
+                            <table class="table-auto border border-ykgray rounded-xl overflow-hidden">
+                                <tbody>
+                                    <tr class="bg-ykwhite text-ykpurple text-left">
+                                        <td class="w-1/7 px-4 py-3"><span class="bg-ykred text-ykwhite rounded-xl px-2 py-1">1</span></td>
+                                        <td class="w-1/7 px-4 py-3">Transaction Name</td>
+                                        <td class="w-1/7 px-4 py-3">Edit / Delete</td>
+                                        <td class="w-1/7 px-4 py-3">Transaction Category</td>
+                                        <td class="w-1/7 px-5 py-3">-Php1,500.00</td>
+                                        <td class="w-1/7 px-4 py-3">Up/Down</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <!--END OF TABLE 1-->
+                        </div>
+                    </div>
+                    <!--END OF COLUMN 1-->
                 </div>
-                <!--Column 1-->
-                <div class="px-5 h-96 max-h-full overflow-y-auto">
-                    <div class="grid grid-cols-1 gap-3 grid-flow-row text-yklightpurple px-5 pt-2 w-auto overflow-auto">
-                        <!--Table 1-->
-                        <div class="py-2 flex"> <!--WEBPAGE TITLE-->
-                            <span class="text-xl font-bold text-ykpurple">April XX, XXXX</span>
+            <!--COLUMN 2-->
+                <div class="w-1/5 bg-ykgray rounded-xl py-6">
+                    <div class="flex-3 pl-4 py-2 justify-start">
+                        <h1 class="text-3xl font-bold text-ykpurple">New Transaction</h1>
+                    </div>
+                    <div class="gap-2">
+                        <div class="grid grid-cols-1 pl-10 py-1 mr-10">
+                            <label for="E_Name" class="text-xl font-medium text-ykpurple">Transaction Name:</label>
+                            <input type="text" id="E_Name" class="border border-ykpurple p-2 rounded-lg text-ykpurple">
                         </div>
-                        <table class="table-auto border border-ykgray rounded-xl overflow-hidden">
-                            <tbody>
-                                <tr class="bg-ykwhite text-ykpurple text-left">
-                                    <td class="w-1/7 px-4 py-3"><span class="bg-ykgreen text-ykwhite rounded-xl px-2 py-1">6</span></td>
-                                    <td class="w-1/7 px-4 py-3">Transaction Name</td>
-                                    <td class="w-1/7 px-4 py-3">Edit / Delete</td>
-                                    <td class="w-1/7 px-4 py-3">Transaction Category</td>
-                                    <td class="w-1/7 px-4 py-3">+Php15,000.00</td>
-                                    <td class="w-1/7 px-4 py-3">Up/Down</td>
-                                </tr>
-                                <tr class="bg-ykwhite text-ykpurple text-left">
-                                    <td class="w-1/7 px-4 py-3"><span class="bg-ykred text-ykwhite rounded-xl px-2 py-1">5</span></td>
-                                    <td class="w-1/7 px-4 py-3">Transaction Name</td>
-                                    <td class="w-1/7 px-4 py-3">Edit / Delete</td>
-                                    <td class="w-1/7 px-4 py-3">Transaction Category</td>
-                                    <td class="w-1/7 px-5 py-3">-Php1,000.00</td>
-                                    <td class="w-1/7 px-4 py-3">Up/Down</td>
-                                </tr>
-                                <tr class="bg-ykwhite text-ykpurple text-left">
-                                    <td class="w-1/7 px-4 py-3"><span class="bg-ykred text-ykwhite rounded-xl px-2 py-1">4</span></td>
-                                    <td class="w-1/7 px-4 py-3">Transaction Name</td>
-                                    <td class="w-1/7 px-4 py-3">Edit / Delete</td>
-                                    <td class="w-1/7 px-4 py-3">Transaction Category</td>
-                                    <td class="w-1/7 px-5 py-3">-Php1,500.00</td>
-                                    <td class="w-1/7 px-4 py-3">Up/Down</td>
-                                </tr>
-                            </tbody>
-                            
-                        </table>
-                        <!--END OF TABLE 1-->
-                        <!--Table 1-->
-                        <div class="py-2 flex"> <!--WEBPAGE TITLE-->
-                            <span class="text-xl font-bold text-ykpurple">March XX, XXXX</span>
+                        <div class="grid grid-cols-1 pl-10 py-3 mr-10">
+                            <label for="M_amount" class="text-xl font-medium text-ykpurple">Amount:</label>
+                            <input type="text" id="M_amount" class="border border-ykpurple p-2 rounded-lg text-ykpurple">
                         </div>
-                        <table class="table-auto border border-ykgray rounded-xl overflow-hidden">
-                            <tbody>
-                                <tr class="bg-ykwhite text-ykpurple text-left">
-                                    <td class="w-1/7 px-4 py-3"><span class="bg-ykgreen text-ykwhite rounded-xl px-2 py-1">3</span></td>
-                                    <td class="w-1/7 px-4 py-3">Transaction Name</td>
-                                    <td class="w-1/7 px-4 py-3">Edit / Delete</td>
-                                    <td class="w-1/7 px-4 py-3">Transaction Category</td>
-                                    <td class="w-1/7 px-4 py-3">+Php15,000.00</td>
-                                    <td class="w-1/7 px-4 py-3">Up/Down</td>
-                                </tr>
-                                <tr class="bg-ykwhite text-ykpurple text-left">
-                                    <td class="w-1/7 px-4 py-3"><span class="bg-ykred text-ykwhite rounded-xl px-2 py-1">2</span></td>
-                                    <td class="w-1/7 px-4 py-3">Transaction Name</td>
-                                    <td class="w-1/7 px-4 py-3">Edit / Delete</td>
-                                    <td class="w-1/7 px-4 py-3">Transaction Category</td>
-                                    <td class="w-1/7 px-5 py-3">-Php1,000.00</td>
-                                    <td class="w-1/7 px-4 py-3">Up/Down</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <!--END OF TABLE 1-->
-                        <!--Table 1-->
-                        <div class="py-2 flex"> <!--WEBPAGE TITLE-->
-                            <span class="text-xl font-bold text-ykpurple">February XX, XXXX</span>
+                        <div class="grid grid-cols-1 pl-10 py-1 mr-10">
+                            <label for="M_amount" class="text-xl font-medium text-ykpurple">Category:</label>
+                            <select id="RemindMe" class="border-ykpurple p-2 rounded-lg">
+                                <option value="" disabled selected>Choose a category:</option>
+                                <option value="option1">Option 1</option>
+                                <option value="option2">Option 2</option>
+                                <option value="option3">Option 3</option>
+                            </select>
                         </div>
-                        <table class="table-auto border border-ykgray rounded-xl overflow-hidden">
-                            <tbody>
-                                <tr class="bg-ykwhite text-ykpurple text-left">
-                                    <td class="w-1/7 px-4 py-3"><span class="bg-ykred text-ykwhite rounded-xl px-2 py-1">1</span></td>
-                                    <td class="w-1/7 px-4 py-3">Transaction Name</td>
-                                    <td class="w-1/7 px-4 py-3">Edit / Delete</td>
-                                    <td class="w-1/7 px-4 py-3">Transaction Category</td>
-                                    <td class="w-1/7 px-5 py-3">-Php1,500.00</td>
-                                    <td class="w-1/7 px-4 py-3">Up/Down</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <!--END OF TABLE 1-->
+                        <div class="flex justify-center p-5">
+                            <button class="bg-ykpurple text-ykwhite font-bold py-2 px-4 rounded mr-3">Add</button>
+                        </div>
+                        
                     </div>
                 </div>
-                <!--END OF COLUMN 1-->
+            
             </div>
-          <!--COLUMN 2-->
-            <div class="w-1/5 bg-ykgray rounded-xl py-6">
-                <div class="flex-3 pl-4 py-2 justify-start">
-                    <h1 class="text-3xl font-bold text-ykpurple">New Transaction</h1>
-                </div>
-                <div class="gap-2">
-                    <div class="grid grid-cols-1 pl-10 py-1 mr-10">
-                        <label for="E_Name" class="text-xl font-medium text-ykpurple">Transaction Name:</label>
-                        <input type="text" id="E_Name" class="border border-ykpurple p-2 rounded-lg text-ykpurple">
-                    </div>
-                    <div class="grid grid-cols-1 pl-10 py-3 mr-10">
-                        <label for="M_amount" class="text-xl font-medium text-ykpurple">Amount:</label>
-                        <input type="text" id="M_amount" class="border border-ykpurple p-2 rounded-lg text-ykpurple">
-                    </div>
-                    <div class="grid grid-cols-1 pl-10 py-1 mr-10">
-                        <label for="M_amount" class="text-xl font-medium text-ykpurple">Category:</label>
-                        <select id="RemindMe" class="border-ykpurple p-2 rounded-lg">
-                            <option value="" disabled selected>Choose a category:</option>
-                            <option value="option1">Option 1</option>
-                            <option value="option2">Option 2</option>
-                            <option value="option3">Option 3</option>
-                        </select>
-                    </div>
-                    <div class="flex justify-center p-5">
-                        <button class="bg-ykpurple text-ykwhite font-bold py-2 px-4 rounded mr-3">Add</button>
-                    </div>
-                    
-                </div>
-            </div>
-          
-          </div>
-        <!--End of Transaction Content-->
-       
+            <!--End of Transaction Content-->
+       </div>
     </div>
         
 </div>
